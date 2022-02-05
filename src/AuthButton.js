09f6fa904ddb3a581eaacf8a850198c698be05ec
Dan Possehl <danielbpossehl@gmail.com>
2022-02-05 11:19:57 -0800

    switch statment finished

--- src/AuthButton.js
+++ src/AuthButton.js
@@ -3,10 +3,21 @@ import React from "react";
 const AuthButton = (props) => {
   let { isLoggedIn } = props;
 
-  if (isLoggedIn) {
-    return <button>Logout</button>;
-  } else {
-    return <button>Login</button>;
+  // if (isLoggedIn) {
+  //   return <button>Logout</button>;
+  // } else {
+  //   return <button>Login</button>;
+  // }
+
+  switch (isLoggedIn) {
+    case true:
+      return <button>Logout</button>;
+      break;
+    case false:
+      return <button>Login</button>;
+      break;
+    default:
+      return null;
   }
 };
 
