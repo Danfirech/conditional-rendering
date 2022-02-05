7563208a7250a43085f44a8b183bfd6e66fb1a8b
Dan Possehl <danielbpossehl@gmail.com>
2022-02-05 11:04:39 -0800

    created login and logout if statement

--- src/App.js
+++ src/App.js
@@ -10,14 +10,21 @@ class App extends Component {
   }
 
   render() {
+    let { isLoggedIn } = this.state;
+
+    const renderAuthButton = () => {
+      if (isLoggedIn) {
+        return <button>Logout</button>;
+      } else return <button>login</button>;
+    };
+
     return (
       <div className="App">
         <h1>
           This is a Demo showing several ways to implement Conditional Rendering
           in React.
         </h1>
-        <button>Login</button>
-        <button>Logout</button>
+        {renderAuthButton()}
       </div>
     );
   }
